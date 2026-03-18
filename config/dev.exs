d7a84b76f455be50d403f7301b3e6c40bfe954f6
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2026-03-19 07:54:57 +0900

    外部IPを許可

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,7 @@ config :blood_pressure_record, BloodPressureRecord.Repo,
 config :blood_pressure_record, BloodPressureRecordWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}],
+  http: [ip: {0, 0, 0, 0}],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
